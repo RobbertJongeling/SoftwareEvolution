a3c4f83541f84871e1f4a5a84c579306b17a2cb6
RobbertJongeling <r.m.jongeling@student.tue.nl>
2014-03-30 18:16:25 +0200

    beginnetje section over R
    en nog wat stuff

--- assignment4/R/Rtest.R
+++ assignment4/R/Rtest.R
@@ -2,7 +2,7 @@
 library("epitools")
 
 #read input
-dataFrame = read.csv(file="D:\\Documents\\github\\SoftwareEvolution\\assignment4\\output.csv", header=TRUE,sep=',')
+dataFrame = read.csv(file="D:\\Documents\\github\\SoftwareEvolution\\assignment4\\R\\test.csv", header=TRUE,sep=',')
 
 #function doing all the work
 f <- function(r) {
@@ -23,7 +23,7 @@ f <- function(r) {
 	
 	#perform output
 	#print(paste(r["owner"], r["name"], pofchisq, res[1,1], res[1,2], res[2,1], res[2,2], oddsRatio, pValue, sep=","))
-	cat(paste(r["owner"], r["name"], pofchisq, res[1,1], res[1,2], res[2,1], res[2,2], oddsRatio, pValue, sep=","), file="D:\\Documents\\github\\SoftwareEvolution\\assignment4\\R\\Routput.csv", append = T, fill = T)
+	cat(paste(r["owner"], r["name"], pofchisq, res[1,1], res[1,2], res[2,1], res[2,2], oddsRatio, pValue, sep=","), file="D:\\Documents\\github\\SoftwareEvolution\\assignment4\\R\\Routputtest.csv", append = T, fill = T)
 }
 
 #applying the function to the input
