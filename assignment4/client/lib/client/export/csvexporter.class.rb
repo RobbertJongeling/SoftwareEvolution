5d826d2325528bea864b354b1f0f1fdde50e5e1b
Tim van Dalen <Tim@timvdalen.nl>
2014-03-30 16:14:17 +0200

    Add required output fields

--- assignment4/client/lib/client/export/csvexporter.class.rb
+++ assignment4/client/lib/client/export/csvexporter.class.rb
@@ -9,13 +9,25 @@ module Export
 		
 			CSV.open(@filename, "wb", :write_headers => true, :headers => headers) do |csv|
 				contingencies.each do |contingency|
-					csv << [contingency.project.owner, contingency.project.name, contingency.commit_passed, contingency.commit_failed, contingency.pr_passed, contingency.pr_failed]
+					csv << [
+						contingency.project.owner,
+						contingency.project.name,
+						contingency.commit_passed,
+						contingency.commit_failed,
+						contingency.pr_passed,
+						contingency.pr_failed,
+						contingency.project.travisid,
+						contingency.project.nr_contributors,
+						contingency.project.nr_changes,
+						contingency.project.age,
+						contingency.project.lang
+					]
 				end
 			end
 		end
 		
 		def headers
-			["owner", "name", "commit_passed", "commit_failed", "pr_passed", "pr_failed"]
+			["owner", "name", "commit_passed", "commit_failed", "pr_passed", "pr_failed", "travis_id", "nr_contributors", "nr_changes", "age", "lang"]
 		end
 	end
 end
